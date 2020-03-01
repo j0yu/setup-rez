71ea7ea23166313f92deb696d919f7631ca957c0
Joseph Yu <josephyu2712+github@gmail.com>
2020-03-01 21:43:15 +0000

    Using our getInstallPy

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
+const fs = require('fs');
 const path = require('path');
 const process = require('process');
 const tc = require('@actions/tool-cache');
+
+
+async function getInstallPy(extractedPath) {
+    // Tarball extracts to repo name + git ref sub-folder
+    const extractDir = await fs.promises.opendir(extractedPath);
+
+    for await (const dirent of extractDir) {
+        if (dirent.isDirectory()) {
+            srcFolder = path.join(extractedPath, dirent.name);
+            const srcDir = await fs.promises.opendir(srcFolder);
+            
+            for await (const srcDirent of srcDir) {
+                if (srcDirent.name == 'install.py') {
+                    return path.join(srcDir, srcDirent.name)
+                }
+            }
+        }
+    }
+    return '';
+}
 
 async function run() {
     try {
@@ -13,9 +34,10 @@ async function run() {
 
         if (!cachedRezPath.length) {
             const rezTarPath = await tc.downloadTool(`https://github.com/${rezGitRepo}/archive/${gitRef}.tar.gz`);
-            const rezSrcPath = await tc.extractTar(rezTarPath, 'src');
-            const rezInstallPath = path.dirname(rezSrcPath);
-            await exec.exec('python', [path.join(rezSrcPath, 'install.py'), rezInstallPath]);
+            const rezInstallPath = await tc.extractTar(rezTarPath);
+            const rezInstallPy = await getInstallPy(rezInstallPath)
+
+            await exec.exec('python', [rezInstallPy, rezInstallPath]);
         
             cachedRezPath = await tc.cacheDir(rezInstallPath, rezGitRepo, gitRef);
         }
